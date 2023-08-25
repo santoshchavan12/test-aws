a88edb9982ea7d6d439f6c4d34606d9ac976a6ab
santoshchavan12 <santhusyc123@gmail.com>
2023-08-25 06:57:39 +0000

    first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 // Your First Program
 import java.util.*;
 class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World! \n changes are made!!"); 
+        System.out.println("Hi Java!!");
+	    System.out.println("Hello, World! \n changes are made!!"); 
     }
 }
